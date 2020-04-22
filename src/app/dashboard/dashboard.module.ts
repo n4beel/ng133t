4df06e5b1a617289a3ebac1458d4c28b14cbad67
Nabeel <na268666@gmail.com>
2020-04-22 05:45:41 +0500

    statistics bar done

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { OverviewComponent } from './overview/overview.component';
 import { DashboardComponent } from './dashboard.component';
+import { StatsBarComponent } from './stats-bar/stats-bar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [DashboardComponent, OverviewComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  declarations: [DashboardComponent, OverviewComponent, StatsBarComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+  ],
 })
 export class DashboardModule {}
